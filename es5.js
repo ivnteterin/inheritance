0d84cda9999d2af2ed42e6fbc2f3af58e11ccceb
ivnteterin <spy930712@gmail.com>
2022-12-31 13:33:13 +0200

    feat: add value check and clean code with prettier

--- es5.js
+++ es5.js
@@ -1,84 +1,89 @@
-function Builder (value) {
+function Builder(value) {
   this.value = value;
-  this.plus = function(...args) {
-    this.value=args.reduce((sum,arg)=> sum+arg,this.value);
+  this.plus = function (...args) {
+    this.value = args.reduce((sum, arg) => sum + arg, this.value);
     return this;
-  }
-  this.minus = function() {};
-  this.multiply = function(){};
-  this.divide = function() {};
-  this.get = function () {return this.value;}
+  };
+  this.minus = function () {};
+  this.multiply = function () {};
+  this.divide = function () {};
+  this.get = function () {
+    return this.value;
+  };
 }
 
 function StringBuilder(value) {
-  Builder.call(this,value);
+  Builder.call(this, value ?? '');
 
-  this.minus = function(n) {
-    this.value = this.value.substring(n,-1);
+  this.minus = function (n) {
+    this.value = this.value.substring(n, -1);
     return this;
-  } 
-  this.multiply = function(n) {
+  };
+  this.multiply = function (n) {
     const arr = [];
-    while (arr.length<n) { arr.push(this.value); }
-    this.value=arr.join(" ");
+    while (arr.length < n) {
+      arr.push(this.value);
+    }
+    this.value = arr.join(' ');
     return this;
-  } 
+  };
 
-  this.divide = function(n) {
-    this.value = this.value.substring(0,Math.floor(this.value.length / n));
+  this.divide = function (n) {
+    this.value = this.value.substring(0, Math.floor(this.value.length / n));
     return this;
-  } 
-  this.remove = function(n) {
-    const arr = this.value.split("");
-    this.value = arr.filter(el => el != n).join("");
+  };
+  this.remove = function (n) {
+    const arr = this.value.split('');
+    this.value = arr.filter((el) => el != n).join('');
     return this;
-  } 
-  this.sub = function(from,n) {
-    this.value = this.value.substring(from+1,n);
+  };
+  this.sub = function (from, n) {
+    this.value = this.value.substring(from + 1, n);
     return this;
-  } 
+  };
 }
 
 function IntBuilder(value) {
-  Builder.call(this, value || 0);
+  Builder.call(this, !isNaN(value) ? +value : 0);
 
-  this.minus = function(...args) {
-    this.value=args.reduce((sum,arg)=> sum-arg,this.value);
+  this.minus = function (...args) {
+    this.value = args.reduce((sum, arg) => sum - arg, this.value);
     return this;
-  }
+  };
 
-  this.multiply = function(n) {
-    this.value=this.value*n;
+  this.multiply = function (n) {
+    this.value = this.value * n;
     return this;
-  }
+  };
 
-  this.divide = function(n) {
-    this.value=this.value/n;
+  this.divide = function (n) {
+    this.value = this.value / n;
     return this;
-  }
+  };
 
-  this.mod = function(n) {
-    this.value=this.value%n;
+  this.mod = function (n) {
+    this.value = this.value % n;
     return this;
-  }
+  };
 }
 
-const newStr = new StringBuilder("Hello");
+const newStr = new StringBuilder('Hello');
 
-console.log("//ES5");
-console.log("StringBuilder result: " + newStr.plus(" all","!")
-                  .minus(4)
-                  .multiply(3)
-                  .divide(4)
-                  .remove("l")
-                  .sub(1,1)
-                  .get());
+console.log('//ES5');
+console.log(
+  'StringBuilder result: ' +
+    newStr
+      .plus(' all', '!')
+      .minus(4)
+      .multiply(3)
+      .divide(4)
+      .remove('l')
+      .sub(1, 1)
+      .get()
+);
 
-const newInt = new IntBuilder(10);
-console.log("IntBuilder result: " + newInt.plus(2, 3, 2)                   
-                  .minus(1, 2)                     
-                  .multiply(2)                   
-                  .divide(4)                       
-                  .mod(3)                          
-                  .get());
-
+const newInt = new IntBuilder();
+console.log(
+  'IntBuilder result: ' +
+    newInt.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get()
+);
--- es6.js
+++ es6.js
@@ -1,101 +1,102 @@
 class Builder {
   constructor(value) {
-    this.value=value;
+    this.value = value;
   }
 
-  plus (...args) {
-    this.value=args.reduce((sum,arg)=> sum+arg,this.value);
+  plus(...args) {
+    this.value = args.reduce((sum, arg) => sum + arg, this.value);
     return this;
   }
-  minus() {};
-  multiply(){};
-  divide() {};
+  minus() {}
+  multiply() {}
+  divide() {}
 
-  get () { return this.value; }
+  get() {
+    return this.value;
+  }
 }
 
 class IntBuilder extends Builder {
   constructor(value) {
-    super(value);
+    super(!isNaN(value) ? +value : 0);
   }
 
-  minus (...args) {
-    this.value=args.reduce((sum,arg)=> sum-arg,this.value);
+  minus(...args) {
+    this.value = args.reduce((sum, arg) => sum - arg, this.value);
     return this;
   }
 
-  multiply (n) {
-    this.value=this.value*n;
+  multiply(n) {
+    this.value = this.value * n;
     return this;
   }
 
-  divide (n) {
-    this.value=this.value/n;
+  divide(n) {
+    this.value = this.value / n;
     return this;
   }
 
-  mod (n) {
-    this.value=this.value%n;
+  mod(n) {
+    this.value = this.value % n;
     return this;
   }
 
-  static random(from,to) {
-   return Math.floor(Math.random() * (to-from) + from);
+  static random(from, to) {
+    return Math.floor(Math.random() * (to - from) + from);
   }
 }
 
 class StringBuilder extends Builder {
   constructor(value) {
-    super(value);
+    super(value ?? '');
   }
 
-  minus (n) {
-    this.value = this.value.substring(n,-1);
+  minus(n) {
+    this.value = this.value.substring(n, -1);
     return this;
-  } 
-  multiply (n) {
+  }
+  multiply(n) {
     const arr = [];
-    while (arr.length<n) { arr.push(this.value); }
-    this.value=arr.join(" ");
+    while (arr.length < n) {
+      arr.push(this.value);
+    }
+    this.value = arr.join(' ');
     return this;
-  } 
+  }
 
   divide(n) {
-    this.value = this.value.substring(0,Math.floor(this.value.length / n));
+    this.value = this.value.substring(0, Math.floor(this.value.length / n));
     return this;
-  } 
+  }
   remove(n) {
-    const arr = this.value.split("");
-    this.value = arr.filter(el => el != n).join("");
+    const arr = this.value.split('');
+    this.value = arr.filter((el) => el != n).join('');
     return this;
-  } 
-  sub(from,n) {
-    this.value = this.value.substring(from+1,n);
+  }
+  sub(from, n) {
+    this.value = this.value.substring(from + 1, n);
     return this;
-  } 
+  }
 }
 
-
-
 const newInt = new IntBuilder(10);
-const newStr = new StringBuilder("Hello");
+const newStr = new StringBuilder('Hello');
 
+console.log('//ES6');
+console.log('Random: ' + IntBuilder.random(10, 100));
+console.log(
+  'StringBuilder result: ' +
+    newStr
+      .plus(' all', '!')
+      .minus(4)
+      .multiply(3)
+      .divide(4)
+      .remove('l')
+      .sub(1, 1)
+      .get()
+);
 
-console.log("//ES6");
-console.log("Random: " +IntBuilder.random(10,100));
-console.log("StringBuilder result: " + newStr.plus(" all","!")
-                  .minus(4)
-                  .multiply(3)
-                  .divide(4)
-                  .remove("l")
-                  .sub(1,1)
-                  .get());
-
-console.log("IntBuilder result: " + newInt.plus(2, 3, 2)                   
-                  .minus(1, 2)                     
-                  .multiply(2)                   
-                  .divide(4)                       
-                  .mod(3)                          
-                  .get());
-
-
+console.log(
+  'IntBuilder result: ' +
+    newInt.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get()
+);
